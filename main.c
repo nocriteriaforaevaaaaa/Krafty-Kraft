e296cc6d334397a3e89c322a047caea45e070344
Adesh-Pandey <75557670+Adesh-Pandey@users.noreply.github.com>
2023-08-16 18:12:44 +0545

    upgrades

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <SDL2/SDL_image.h>
 
-struct drawingStates
+struct DrawingStates
 {
     int r;
     int b;
     int g;
     int a;
     bool isDrawing;
-
+    SDL_Rect point;
 } state;
 
 struct Eraser
@@ -19,6 +20,10 @@ struct Eraser
     bool isErasing;
 
 } eraser;
+
+// handles key down events
+void keyDown(SDL_Keycode *code, struct DrawingStates *state, struct Eraser *eraser);
+
 int main(int argc, char *argv[])
 {
 
@@ -28,6 +33,10 @@ int main(int argc, char *argv[])
         return 0;
     SDL_CreateWindowAndRenderer(700, 700, 0, &window, &renderer);
 
+    SDL_SetWindowTitle(window, "Krafty Kraft");
+    SDL_Surface *icon = IMG_Load("palette.png");
+
+    SDL_SetWindowIcon(window, icon);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
@@ -42,10 +51,13 @@ int main(int argc, char *argv[])
 
     // normal eraser dimentions;
 
-    eraser.block.h = 50;
-    eraser.block.w = 50;
+    eraser.block.h = 20;
+    eraser.block.w = 20;
     eraser.isErasing = false;
     eraser.isEraserOn = false;
+
+    state.point.h = 1;
+    state.point.w = 1;
 
     bool quit = false;
     int mx;
@@ -67,50 +79,13 @@ int main(int argc, char *argv[])
                 break;
 
             case SDL_KEYDOWN:
-                switch (event.key.keysym.sym)
-                {
-                // keys to switch between different helpers
-                case SDLK_e:
-                    eraser.isEraserOn = true;
-                    break;
-                case SDLK_r:
-                    state.a = 255;
-                    state.b = 0;
-                    state.g = 0;
-                    state.r = 255;
-                    break;
 
-                case SDLK_w:
-                    state.a = 255;
-                    state.b = 255;
-                    state.g = 255;
-                    state.r = 255;
-                    break;
-                case SDLK_b:
-                    state.r = 0;
-                    state.b = 255;
-                    state.g = 0;
-                    state.a = 255;
-                    break;
-
-                case SDLK_g:
-                    state.r = 0;
-                    state.b = 0;
-                    state.g = 255;
-                    state.a = 255;
-                    break;
-                }
-                break;
-            // not working so currently using go back to eraser on mouseUp method
-            case SDLK_p:
-                eraser.isEraserOn = false;
-                break;
+                keyDown(&(event.key.keysym.sym), &state, &(eraser));
 
             case SDL_MOUSEBUTTONUP:
                 // clear all states
                 state.isDrawing = false;
                 eraser.isErasing = false;
-                eraser.isEraserOn = false; // hack to fix the eraser glitch
                 break;
 
             case SDL_MOUSEMOTION:
@@ -119,15 +94,16 @@ int main(int argc, char *argv[])
                 if (state.isDrawing)
                 {
                     SDL_SetRenderDrawColor(renderer, state.r, state.g, state.b, state.a);
-                    SDL_RenderDrawPoint(renderer, mx, my);
+                    state.point.x = mx;
+                    state.point.y = my;
+                    SDL_RenderFillRect(renderer, &(state.point));
                     // draw a point on that location
                 }
                 else if (eraser.isErasing && eraser.isEraserOn)
                 {
-
                     // draws a block of defined dimentions
-                    eraser.block.x = mx - 25;
-                    eraser.block.y = my - 25;
+                    eraser.block.x = mx - eraser.block.h / 2;
+                    eraser.block.y = my - eraser.block.w / 2;
                     SDL_SetRenderDrawColor(renderer, 0, 0, 0, state.a);
                     SDL_RenderFillRect(renderer, &(eraser.block));
                 }
@@ -143,4 +119,111 @@ int main(int argc, char *argv[])
     // destroy window and renderer
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+}
+
+void keyDown(SDL_Keycode *code, struct DrawingStates *state, struct Eraser *eraser)
+{
+    switch (*code)
+    {
+    // keys to switch between different helpers
+    case SDLK_e:
+        eraser->isEraserOn = true;
+        break;
+
+    case SDLK_1:
+        if (eraser->isEraserOn)
+        {
+            eraser->block.h = 25;
+            eraser->block.w = 25;
+        }
+        else
+        {
+            state->point.w = 1;
+            state->point.h = 1;
+        }
+        break;
+        break;
+
+    case SDLK_2:
+        if (eraser->isEraserOn)
+        {
+            eraser->block.h = 30;
+            eraser->block.w = 30;
+        }
+        else
+        {
+            state->point.w = 2;
+            state->point.h = 2;
+        }
+        break;
+
+    case SDLK_3:
+        if (eraser->isEraserOn)
+        {
+            eraser->block.h = 35;
+            eraser->block.w = 35;
+        }
+        else
+        {
+            state->point.w = 3;
+            state->point.h = 3;
+        }
+        break;
+
+    case SDLK_4:
+        if (eraser->isEraserOn)
+        {
+            eraser->block.h = 40;
+            eraser->block.w = 40;
+        }
+        else
+        {
+            state->point.w = 4;
+            state->point.h = 4;
+        }
+        break;
+
+    case SDLK_5:
+        if (eraser->isEraserOn)
+        {
+            eraser->block.h = 50;
+            eraser->block.w = 50;
+        }
+        else
+        {
+            state->point.w = 5;
+            state->point.h = 5;
+        }
+        break;
+    case SDLK_r:
+        state->a = 255;
+        state->b = 0;
+        state->g = 0;
+        state->r = 255;
+        break;
+
+    case SDLK_w:
+        state->a = 255;
+        state->b = 255;
+        state->g = 255;
+        state->r = 255;
+        break;
+    case SDLK_b:
+        state->r = 0;
+        state->b = 255;
+        state->g = 0;
+        state->a = 255;
+        break;
+
+    case SDLK_g:
+        state->r = 0;
+        state->b = 0;
+        state->g = 255;
+        state->a = 255;
+        break;
+
+    case SDLK_p:
+        eraser->isEraserOn = false;
+        break;
+    }
 }
